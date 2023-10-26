6f013b4dd72b170c4d0ef7288f870019b1608410
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-10-26 20:03:08 +0100

    add latest projects

---
+++ color_typing_game.py
@@ -0,0 +1,89 @@
+import customtkinter as ctk
+import random
+
+
+COLORS = ["red", "green", "blue", "yellow", "violet", "black", "white"]
+TOTAL_GAME_TIME = 5
+
+time_left = TOTAL_GAME_TIME
+score = 0
+curr_color = random.choice(COLORS)
+
+def reset_game():
+    global time_left, score
+
+    time_left = TOTAL_GAME_TIME
+    score = 0
+
+    timer.configure(text=f"Time left: {time_left}s", text_color='white')
+    user_score.configure(text=f"Score: {score}")
+
+
+def changeText():
+    global curr_color
+
+    curr_color = random.choice(COLORS)
+    changing_text.configure(text=random.choice(COLORS), text_color=curr_color)
+
+def run_timer():
+    global time_left
+
+    if time_left > 0:
+        time_left -= 1
+        timer.configure(text=f"Time left: {time_left}s")
+        root.after(1000, run_timer)
+    elif time_left == 0:
+        timer.configure(text="Time up!", text_color='red')
+        reset_btn.configure(state=ctk.NORMAL)
+
+
+def start_game(event):
+    global score
+
+    if time_left == TOTAL_GAME_TIME:
+        reset_btn.configure(state=ctk.DISABLED)
+        run_timer()
+
+    if time_left > 0:
+        if user_input.get().lower() == curr_color:
+            score += 1
+            user_score.configure(text=f"Score: {score}")
+
+        user_input.delete(0, "end")
+        changeText()
+
+
+root = ctk.CTk()
+root.title("Color Typing Game")
+root.minsize(500, 350)
+
+app_frame = ctk.CTkFrame(root, width=700)
+app_frame.pack(padx=20, pady=20, expand=True, fill=ctk.BOTH)
+
+time_and_score_frame = ctk.CTkFrame(app_frame)
+time_and_score_frame.pack(fill=ctk.X)
+
+timer = ctk.CTkLabel(time_and_score_frame, text=f"Time left: {TOTAL_GAME_TIME}s")
+timer.pack(side="left", padx=10)
+
+user_score = ctk.CTkLabel(time_and_score_frame, text=f"Score: 0")
+user_score.pack(side="right", padx=10)
+
+ctk.CTkLabel(
+    app_frame, text="Instruction: Enter the color of the text you see\nand not the text",
+    font=("Montserrat", 18, "italic"), text_color='lightblue'
+).pack(padx=10, pady=20)
+
+changing_text = ctk.CTkLabel(app_frame, text="blue", font=("Montserrat", 30, "bold"))
+changing_text.pack(pady=20, fill=ctk.X)
+
+user_input = ctk.CTkEntry(app_frame, width=300, height=50)
+user_input.pack()
+user_input.bind("<Return>", start_game)
+
+ctk.CTkLabel(app_frame, text="Press enter to start the game...", text_color="grey").pack()
+
+reset_btn = ctk.CTkButton(app_frame, text='Reset Game', command=reset_game)
+reset_btn.pack(pady=20)
+
+root.mainloop()
---
+++ language_translator.py
@@ -0,0 +1,45 @@
+import customtkinter as ctk
+from deep_translator import GoogleTranslator
+
+
+def translate_language():
+    text_to_trans = user_text.get()
+    lang = lang_to_var.get()
+
+    output = GoogleTranslator(source='auto', target=lang).translate(text_to_trans)
+
+    translated_text.configure(state='normal')
+    translated_text.delete(0, 'end')
+    translated_text.insert(0, output)
+
+
+root = ctk.CTk()
+root.title("Language Translator")
+root.minsize(400, 400)
+
+ctk.CTkLabel(root, text='Language Translator', font=('Arial', 30)).pack(anchor=ctk.CENTER)
+
+app_frame = ctk.CTkFrame(root, width=500, height=500, fg_color='transparent')
+app_frame.pack(fill=ctk.X, padx=20, pady=10)
+
+ctk.CTkLabel(app_frame, text='Enter text to translate here 🔥').pack(expand=True, fill=ctk.X)
+user_text = ctk.CTkEntry(app_frame, height=50)
+user_text.pack(fill=ctk.X)
+
+ctk.CTkLabel(app_frame, text='Choose language to translate to: 📝').pack()
+lang_to_var = ctk.StringVar(value="english")
+langs_list = GoogleTranslator().get_supported_languages()
+lang_to = ctk.CTkOptionMenu(app_frame, values=langs_list, variable=lang_to_var)
+lang_to.set("english")
+lang_to.pack(fill=ctk.X, ipady=5)
+
+ctk.CTkLabel(app_frame, text='Translated text 👇').pack()
+translated_text = ctk.CTkEntry(app_frame, height=100,
+                              placeholder_text='Translated text will show here.',
+                              placeholder_text_color='grey', state=ctk.DISABLED)
+translated_text.pack(fill=ctk.X)
+
+translate_btn = ctk.CTkButton(app_frame, text='Translate', command=translate_language)
+translate_btn.pack(fill=ctk.X, pady=20)
+
+root.mainloop()
